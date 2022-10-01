36a02dc89cc8d46d96f9854cd1336c40eed10096
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-01 16:55:59 +0300

    Changes

--- Seminars/Seminar_1/Task_3/Program.cs
+++ Seminars/Seminar_1/Task_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <= n)
+{
+    Console.WriteLine(count);
+    count = count +1;
+}
