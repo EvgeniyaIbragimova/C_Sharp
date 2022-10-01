ab52b9f85297a6440baa9f990cbafc5c9841c666
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-01 17:03:08 +0300

    Tusk_2

--- Seminars/Seminar_1/Task_2/Program.cs
+++ Seminars/Seminar_1/Task_2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+ 
