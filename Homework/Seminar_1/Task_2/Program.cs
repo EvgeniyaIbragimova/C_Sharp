6994e9652aabd960108354e0c9b2311663ece6ca
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-02 15:31:54 +0300

    Homework/Seminar_1/Task_2 done

--- Homework/Seminar_1/Task_2/Program.cs
+++ Homework/Seminar_1/Task_2/Program.cs
@@ -8,8 +8,9 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
-if (b > a) max = b;
-if (c > b) max = c;
+if (b > max) max = b;
+if (c > max) max = c;
+if (a > max) max = a;
 {
     Console.WriteLine($"max = {max}");
 }
