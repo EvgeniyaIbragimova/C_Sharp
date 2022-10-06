173b528cf2e35b0cd02e7d7bb4335c5526a87bff
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-06 13:45:14 +0300

    Homework/Seminar_2/Task_1 done

--- Homework/Seminar_2/Task_1/Program.cs
+++ Homework/Seminar_2/Task_1/Program.cs
@@ -3,3 +3,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 & number < 1000)
+{
+    int result1 = number / 10;
+    int result2 = result1 % 10;
+    System.Console.WriteLine(result2);
+} 
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
