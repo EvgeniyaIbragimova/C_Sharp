e75f9069b3272a46b83ef8634e2b0366b924283a
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-06 15:08:39 +0300

    Changes

--- Homework/Seminar_2/Task_2/Program.cs
+++ Homework/Seminar_2/Task_2/Program.cs
@@ -3,3 +3,13 @@
 // 456 -> 46
 // 782 -> 72
 // 918 -> 98
+
+int number = new Random().Next(100, 1000);            
+int Compound(int number)                    
+{
+    int number3 = number % 10;                                     
+    int number1 = number / 100 % 10;                   
+    int result = number1 * 10 + number3;               
+    return result;                                   
+}
+System.Console.WriteLine($"{number} -> {Compound(number)}");
