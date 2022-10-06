f68a68a326ff8c8f3583dce8db5016be47562f07
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-06 16:37:47 +0300

    Seminar_2/Task_4 done

--- Homework/Seminar_2/Task_4/Program.cs
+++ Homework/Seminar_2/Task_4/Program.cs
@@ -4,6 +4,19 @@
 // 1 -> нет
 
 System.Console.WriteLine("Введите число: ");
-
-
-
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7)
+{
+    System.Console.WriteLine("Такого дня недели не существует");
+}
+else
+{
+    if (day < 6)
+    {
+        System.Console.WriteLine("Нет");
+    }
+    else
+    {
+        System.Console.WriteLine("Да");
+    }
+}
