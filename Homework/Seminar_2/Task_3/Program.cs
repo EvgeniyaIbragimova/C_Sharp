0d4c054872dccf1950a5d05bcc25b44d74531f05
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-06 16:06:33 +0300

    Seminar_2/Task_3 done

--- Homework/Seminar_2/Task_3/Program.cs
+++ Homework/Seminar_2/Task_3/Program.cs
@@ -5,4 +5,22 @@
 // 326792 -> 6
 
 System.Console.WriteLine("Введите число: ");
-
+int number = Convert.ToInt32(Console.ReadLine());
+int Compound(int number)
+{
+    int a = number % 10;
+    int result = a;
+    return result;
+}
+if (number > 99)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    System.Console.WriteLine(Compound(number));
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
