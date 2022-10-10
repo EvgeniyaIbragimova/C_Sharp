80b9fd12768a3e94931a77b028c7706b78cfe6a4
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-10 16:26:08 +0300

    Homework/Seminar_3/Task_2 done

--- Homework/Seminar_3/Task_2/Program.cs
+++ Homework/Seminar_3/Task_2/Program.cs
@@ -2,76 +2,25 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// const int X = 0;
-// const int Y = 0;
-// const int Z = 0;
-
-// int Prompt(string msg)
-// {
-//     System.Console.Write(msg);
-//     int number = Convert.ToInt32(Console.ReadLine());
-//     return number;
-// }
-
-// int[] InputPoint(int point)
-// {
-//     int[] answer = new int[2];
-//     answer[X] = Prompt($"Введите x {point} -> ");
-//     answer[Y] = Prompt($"Введите y {point} -> ");
-//     answer[Z] = Prompt($"Введите z {point} -> ");
-//     return answer;
-// }
-
-// int Power2(int arg)
-// {
-//     return arg * arg;
-// }
-
-// int[] p1 = InputPoint(1);
-// int[] p2 = InputPoint(2);
-// int[] p3 = InputPoint(3);
-
-// double lenght = Math.Sqrt(Power2(p1[X] - p2[X]) + Power2(p1[Y] - p2[Y]) + Power2(p1[Z] - p2[Z]));
-
-// System.Console.WriteLine($"{lenght:f2}"); // f2 -> количество знаков после запятой
-
-
-
-
-
-
-
-int Prompt(string message)
+int Prompt(string msg)
 {
-    System.Console.Write(message);
-    string readValue = Console.ReadLine();
-    int result = int.Parse(readValue);
-    return result;
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
 }
 
-int x1 = Prompt("Введите значение координаты точки А по оси X -> ");
-int y1 = Prompt("                                    по оси Y -> ");
-int z1 = Prompt("                                    по оси Z -> ");
-int x2 = Prompt("Введите значение координаты точки B по оси X -> ");
-int y2 = Prompt("                                    по оси Y -> ");
-int z2 = Prompt("                                    по оси Z -> ");
+int x1 = Prompt("Введите координаты точки А: X1 -> ");
+int y1 = Prompt("Введите координаты точки А: Y1 -> ");
+int z1 = Prompt("Введите координаты точки А: Z1 -> ");
+int x2 = Prompt("Введите координаты точки B: X2 -> ");
+int y2 = Prompt("Введите координаты точки А: Y2 -> ");
+int z2 = Prompt("Введите координаты точки А: Z2 -> ");
 
-double VectorLength()
+int Power(int arg)
 {
-    double length = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
-    return length;
+    return arg * arg;
 }
 
-double length = VectorLength();
-Console.Write($"A ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {length}");
-Console.WriteLine();
+double length = Math.Sqrt(Power(x1 - x2) + Power(y1 - y2) + Power(z1 - z2));
 
-
-
-
-
-
-
-
-
-
+System.Console.WriteLine($"A ({x1},{y1},{z1}); B ({x2},{y2},{z2}) -> {length:f2}");
