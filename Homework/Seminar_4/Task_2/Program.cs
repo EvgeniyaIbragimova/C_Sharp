efc54e87618e6513386843addd1ab864485153ce
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-14 13:47:54 +0300

    Seminar_4/Task_2 done

--- Homework/Seminar_4/Task_2/Program.cs
+++ Homework/Seminar_4/Task_2/Program.cs
@@ -5,46 +5,28 @@
 // 82 -> 10
 // 9012 -> 12
 
+int Prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
 
+int number = Prompt("Введите число: ");
 
+int Sum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int Prompt(string message)
-// {
-//     Console.Write(message);
-//     string readValue = Console.ReadLine();
-//     int result = int.Parse(readValue);
-//     return result;
-// }
- 
-// int SumOfDigitsOfNumber(int number)
-// {
-//     int result = 0;
-//     while (number > 0)
-//     {
-//         result += number % 10;
-//         number = number / 10;
-//     }
-//     return result;
-// }
-
-// int value = Prompt("Введите значение числа -> ");
-// if (value >= 0)
-//     {
-//     System.Console.WriteLine($"{value} -> {SumOfDigitsOfNumber(value)}");
-//     }
-// else System.Console.WriteLine("Некорректный ввод, введите положительное число");  
+if (number >= 0)
+{
+    System.Console.WriteLine($"{number} -> {Sum(number)}");
+}
+else System.Console.WriteLine("Введите положительное число");
