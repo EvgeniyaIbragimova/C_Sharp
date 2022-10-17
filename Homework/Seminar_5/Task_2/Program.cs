9d36f99ce90902846491f6a0c82e56c81fac33d4
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-17 15:08:42 +0300

    Homework/Seminar_5/Task_2 done

--- Homework/Seminar_5/Task_2/Program.cs
+++ Homework/Seminar_5/Task_2/Program.cs
@@ -3,3 +3,40 @@
 // [3, 7, 23, 12] -> 26
 // [-4, -6, 4, 67] -> 0
 
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] myArray = CreateArray(5, 1, 10);
+PrintArray(myArray);
+
+int SumPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма элементов массива, стоящих на нечетных позициях -> {SumPosition(myArray)}");
