e0c9d26ba08580ce5422b625ae7a191b4fff8e6d
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-17 14:47:47 +0300

    Homework/Seminar_5/Task_1 done

--- Homework/Seminar_5/Task_1/Program.cs
+++ Homework/Seminar_5/Task_1/Program.cs
@@ -3,3 +3,41 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] myArray = CreateArray(5, 100, 1000);
+PrintArray(myArray);
+
+int CountNumbers(int[] array)
+{
+    int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+System.Console.WriteLine($"Количество четных чисел: {CountNumbers(myArray)}");
