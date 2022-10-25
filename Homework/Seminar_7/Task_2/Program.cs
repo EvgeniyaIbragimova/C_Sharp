03eb7d21397cc3df5871487ff509460cf8839058
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-25 17:32:35 +0300

    Seminar_7/Task_2 done

--- Homework/Seminar_7/Task_2/Program.cs
+++ Homework/Seminar_7/Task_2/Program.cs
@@ -8,4 +8,55 @@
 
 // 1, 7 -> такого числа в массиве нет
 
+int[,] CreateArray(int lenRow, int lenColumns)
+{
+    int[,] array = new int[lenRow, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 9);
+        }
+    }
+    return array;
+}
 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindPosition(int[,] matrix, int value1, int value2)
+{
+    int j = value1;
+    int i = value2;
+    if (i >= matrix.GetLength(0) || i < 0 || j >= matrix.GetLength(1) || j < 0)
+    {
+        Console.WriteLine($"{value1} , {value2} -> такого числа в массиве нет");
+    }
+    else
+    {
+        Console.WriteLine($"{value1}, {value2} -> {matrix[i, j]}");
+    }
+}
+
+int[,] array = CreateArray(3, 3);
+PrintArray(array);
+int number1 = Prompt("Введите номер строки: ");
+int number2 = Prompt("Введите номер столбца: ");
+FindPosition(array, number1, number2);
