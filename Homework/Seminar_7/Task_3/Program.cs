36fc13e9da133cd048a4ec21fff151fd377048ad
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-25 18:15:53 +0300

    Seminar_7/Task_3 done

--- Homework/Seminar_7/Task_3/Program.cs
+++ Homework/Seminar_7/Task_3/Program.cs
@@ -8,3 +8,48 @@
 
 // Среднее арифметическое каждого СТОЛБЦА: 4,6; 5,6; 3,6; 3.
 
+int[,] CreateArray(int lenRow, int lenColumns)
+{
+    int[,] array = new int[lenRow, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindAverage(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        double average = sum / array.GetLength(0);
+        Console.Write("{0,7:F1}", average);
+    }
+}
+
+int[,] array = CreateArray(3, 4);
+PrintArray(array);
+Console.WriteLine("Среднее арифметическое по столбцам: ");
+FindAverage(array);
+Console.WriteLine();
