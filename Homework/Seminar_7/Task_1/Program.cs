f22ca249a2ef8f5520977b83f393f27ab78b0f1f
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-10-25 16:50:25 +0300

    Seminar_7/Task_1 done

--- Homework/Seminar_7/Task_1/Program.cs
+++ Homework/Seminar_7/Task_1/Program.cs
@@ -4,3 +4,32 @@
 // 0,5    7    -2   -0,2
 // 1   -3,3     8   -9,9
 // 8    7,8  -7,1      9
+
+double[,] CreateArray(int lenRow, int lenColumns)
+{
+    double[,] array = new double[lenRow, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 6) + random.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:f2}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array = CreateArray(3, 4);
+PrintArray(array);
