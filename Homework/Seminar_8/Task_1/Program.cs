313add173f529cc065ef628b10b6a1f565de1fa9
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-11-12 14:51:39 +0300

    Seminar_8

---
+++ Homework/Seminar_8/Task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: 
+// Задайте двумерный массив. 
+// Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+
+int[,] CreateArray(int lenRow, int lenColumns)
+{
+    int[,] array = new int[lenRow, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 6);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] DescendingSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+                if (array[i, k] > array[i, j])
+                {
+                    int temporary = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temporary;
+                }
+        }
+    }
+    return array;
+}
+
+int[,] array = CreateArray(4, 4);
+PrintArray(array);
+System.Console.WriteLine();
+int[,] sort = DescendingSort(array);
+PrintArray(sort);
