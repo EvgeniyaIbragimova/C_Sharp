f2fd63f44c5e40a9cc89208dfceaa892122aee71
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-11-12 14:57:40 +0300

    Seminar_8/Task_1 done

---
+++ Homework/Seminar_8/Task_2/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 2: 
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] CreateArray(int lenRow, int lenColumns)
+{
+    int[,] array = new int[lenRow, lenColumns];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 6);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FindMinSum(int[,] matrix)
+{
+    int index = 0;
+    int minSum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Строка {i} -> сумма элементов {sum}");
+        if (i == 0)
+        {
+            minSum = sum;
+        }
+        else if (sum < minSum)
+        {
+            minSum = sum;
+            index = i;
+        }
+    }
+    string row = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        row += matrix[index, j] + " ";
+    }
+    System.Console.WriteLine();
+    Console.WriteLine($"Строка с наименьшей суммой элементов -> строка {index}");
+}
+
+int[,] array = CreateArray(4, 4);
+PrintArray(array);
+System.Console.WriteLine();
+FindMinSum(array);
