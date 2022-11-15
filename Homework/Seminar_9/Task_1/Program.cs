449589aa644c0d602196e3f7739a87cd43ddb0d1
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-11-15 15:55:51 +0300

    Homework/Seminar_9

---
+++ Homework/Seminar_9/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1: 
+// Задайте значения M и N. 
+// Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FindNaturalNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return;
+    }
+    if (numberM % 2 == 0)
+    {
+        Console.Write($"{numberM}\t");
+    }
+    FindNaturalNumbers(numberM + 1, numberN);
+}
+
+int numberM = Prompt("Введите число M -> ");
+int numberN = Prompt("Введите число N -> ");
+if (numberM > numberN)
+{
+    Console.WriteLine($"Число M не должно быть больше числа N");
+}
+else
+{
+    FindNaturalNumbers(numberM, numberN);
+}
+Console.WriteLine();
