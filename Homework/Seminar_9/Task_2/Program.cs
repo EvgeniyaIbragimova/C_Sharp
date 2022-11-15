96b725536da3f20e2b0bd652857e0a8a296eb55f
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-11-15 16:00:32 +0300

    Homework/Seminar_9/Task_2 done

--- Homework/Seminar_9/Task_2/Program.cs
+++ Homework/Seminar_9/Task_2/Program.cs
@@ -3,3 +3,30 @@
 // Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int SumNaturalElements(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return 0;
+    }
+    return SumNaturalElements(numberM + 1, numberN) + numberM;
+}
+
+int numberM = Prompt("Введите число M -> ");
+int numberN = Prompt("Введите число N -> ");
+if (numberM > numberN)
+{
+    Console.WriteLine($"Число M не должно быть больше числа N");
+}
+else
+{
+    Console.WriteLine($"M = {numberM}; N = {numberN} -> {SumNaturalElements(numberM, numberN)}");
+}
